370a3ae9bb839ad6c9663291cd5228353c36e75d
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-31 17:11:50 +0100

    Added user sign-up and login functionality

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+require("express-async-errors");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { MONGODB_URI } = require("./utils/config");
+const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
+
+const mongoUrl = MONGODB_URI;
+
+console.log("Connecting to", mongoUrl);
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.tokenExtractor);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ utils/middleware.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
+  }
+
+  next(error);
+};
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
+const tokenExtractor = (request, response, next) => {
+  request.token = getTokenFrom(request);
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  request.user = user;
+  next();
+};
+
+module.exports = {
+  tokenExtractor,
+  userExtractor,
+  unknownEndpoint,
+  errorHandler,
+};
